90e2788f54d924a67481d1b47f0fd8cccd138254
David <59452680+davidrnu@users.noreply.github.com>
2025-04-24 09:14:14 +0000

    unicafe 1.8 done

--- unicafe/src/App.jsx
+++ unicafe/src/App.jsx
@@ -1,42 +1,57 @@
-import { useState } from 'react'
+import { useState } from "react";
 
-const Display = ({type, text}) => type == "title" ? <h1>{text}</h1> : <p>{text}</p>
+const Display = ({ type, text }) =>
+  type == "title" ? <h1>{text}</h1> : <p>{text}</p>;
 
-const Button = ({handleClick, text}) => <button onClick={handleClick}>{text}</button>
+const Button = ({ handleClick, text }) => (
+  <button onClick={handleClick}>{text}</button>
+);
 
-const App = () => {
-  // guarda los clics de cada botón en su propio estado
-  const [good, setGood] = useState(0)
-  const [neutral, setNeutral] = useState(0)
-  const [bad, setBad] = useState(0)
+const Statistics = ({ stats }) => {
+  const good = stats.good;
+  const neutral = stats.neutral;
+  const bad = stats.bad;
 
-  const handleGood = () => setGood(good + 1)
-  const handleNeutral = () => setNeutral(neutral + 1)
-  const handleBad = () => setBad(bad + 1)
-  
-  const all = good + neutral + bad
-  const average = all === 0 ? 0 : (good - bad) / all
-  const positive = all === 0 ? 0 : (good / all) * 100
+  const all = good + neutral + bad;
+  const average = all === 0 ? 0 : (good - bad) / all;
+  const positive = all === 0 ? 0 : (good / all) * 100;
 
   return (
     <div>
-    <Display type="title" text="give feedback" />
+      <Display text={`good ${good}`} />
+      <Display text={`neutral ${neutral}`} />
+      <Display text={`bad ${bad}`} />
 
-    <Button handleClick={handleGood} text="good" />
-    <Button handleClick={handleNeutral} text="neutral" />
-    <Button handleClick={handleBad} text="bad" />
-    
-    <Display type="title" text="statistics" />
-    
-    <Display text={`good ${good}`} />
-    <Display text={`neutral ${neutral}`} />
-    <Display text={`bad ${bad}`} />
-    
-    <Display text={`all ${all}`} />
-    <Display text={`average ${average}`} />
-    <Display text={`positive ${positive}%`} />
+      <Display text={`all ${all}`} />
+      <Display text={`average ${average}`} />
+      <Display text={`positive ${positive}%`} />
     </div>
-  )
-}
+  );
+};
 
-export default App
+const App = () => {
+  // guarda los clics de cada botón en su propio estado
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+
+  const handleGood = () => setGood(good + 1);
+  const handleNeutral = () => setNeutral(neutral + 1);
+  const handleBad = () => setBad(bad + 1);
+
+  return (
+    <div>
+      <Display type="title" text="give feedback" />
+
+      <Button handleClick={handleGood} text="good" />
+      <Button handleClick={handleNeutral} text="neutral" />
+      <Button handleClick={handleBad} text="bad" />
+
+      <Display type="title" text="statistics" />
+
+      <Statistics stats={{ good, neutral, bad }} />
+    </div>
+  );
+};
+
+export default App;
